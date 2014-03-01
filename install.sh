a65ed75a5abb79fa91ecec9cea0ae25f513c705f
Edmundo Fuentes <me@edmundofuentes.com>
2014-03-01 17:49:58 -0600

    Creating installation scripts

--- install.sh
+++ install.sh
@@ -1,38 +1,25 @@
 #!/bin/bash
-# version 1.5 - 2014.03.2
+
+## Assuming a basic LAMP stack running on Ubuntu
 
 echo -e "\e[1m--- Update Repositories ---\e[0m"
 sudo add-apt-repository -y ppa:ondrej/php5-oldstable
 sudo apt-get update > /dev/null
 
-echo -e "\e[1m--- Install Apache2 ---\e[0m"
-sudo apt-get install -y apache2
-sudo a2enmod rewrite
-sudo service apache2 restart
-
-echo -e "\e[1m--- Install PHP5.4 ---\e[0m"
+echo -e "\e[1m--- Update to PHP5.4 ---\e[0m"
 sudo apt-get install -y php5
 sudo apt-get install -y php5-json libapache2-mod-php5
 sudo service apache2 restart
-
-echo -e "\e[1m--- Install PHP5 MySQL Drivers ---\e[0m"
-sudo apt-get install -y php5-mysql
-
-echo -e "\e[1m--- Prepare /var/www ---\e[0m"
-sudo adduser $USER www-data
-sudo chown -R www-data:www-data /var/www
-sudo chmod -R g+rw /var/www
-
-echo -e "\e[1m--- Install MySQL ---\e[0m"
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password devroot'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password devroot'
-sudo apt-get install -y mysql-server
-sudo apt-get install -y php5-mysql
 
 echo -e "\e[1m--- Install cURL ---\e[0m"
 sudo apt-get install -y curl
 sudo apt-get install -y libcurl3 php5-curl php5-intl
 sudo service apache2 restart
+
+echo -e "\e[1m--- Prepare /var/www ---\e[0m"
+sudo adduser $USER www-data
+sudo chown -R www-data:www-data /var/www
+sudo chmod -R g+rw /var/www
 
 echo -e "\e[1m--- Configure Apache & Virtual Hosts ---\e[0m"
 echo - Prepare log files -
@@ -52,28 +39,22 @@ sudo a2ensite dev.com
 sudo a2ensite www.dev.com
 sudo service apache2 restart
 
-echo - Update Hosts file -
-sudo chmod 777 /etc/hosts
-sudo echo "127.0.0.1 dev.com" >> /etc/hosts
-sudo chmod 644 /etc/hosts
-
 echo - Fix Permissions -
 cd ..
 sudo chmod 777 -R www
 cd www
 
-echo -e "\e[1m--- Install Symfony on dev.com ---\e[0m"
-mkdir dev.com
+echo -e "\e[1m--- Install Symfony on triatlonafondo.com ---\e[0m"
+cd triatlonafondo.com
 
 echo - Download Composer -
 curl -sS https://getcomposer.org/installer | php
 php composer.phar config --global discard-changes true
 
-echo - Create Symfony installation -
-php composer.phar create-project --no-interaction symfony/framework-standard-edition dev.com/ 2.4.*
+echo - Install Symfony -
+php composer.phar install
 
 echo - Clean up installation -
-mv composer.phar dev.com/composer.phar
 chmod -R 777 dev.com/app/cache
 chmod -R 777 dev.com/app/logs
 chmod -R 777 dev.com/app/config/parameters.yml
