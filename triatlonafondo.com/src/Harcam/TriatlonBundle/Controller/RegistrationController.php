d28525e44f0883b284b7546dfc23c8378c7cecc1
Edmundo Fuentes <me@edmundofuentes.com>
2014-03-01 19:09:10 -0600

    Adding multiple-choice options to form

--- triatlonafondo.com/src/Harcam/TriatlonBundle/Controller/RegistrationController.php
+++ triatlonafondo.com/src/Harcam/TriatlonBundle/Controller/RegistrationController.php
@@ -15,7 +15,35 @@ class RegistrationController extends Controller
         // Initialize Client object to generate a form
         $client = new Client();
 
+        $distances = array(
+            'SS' => 'Super Sprint'
+        );
+
+        $categories = array(
+            'JF' => 'Femenil 16 a 19 años',
+            'N' => 'Femenil 20 a 24 años',
+            'O' => 'Femenil 25 a 29 años',
+            'P' => 'Femenil 30 a 34 años',
+            'Q' => 'Femenil 35 a 39 años',
+            'R' => 'Femenil 40 a 44 años',
+            'S' => 'Femenil 45 a 49 años',
+            'T' => 'Femenil 50 a 54 años',
+            'V' => 'Femenil 55 a 59 años',
+            'IC' => 'Varonil 14 a 15 años',
+            'JV' => 'Varonil 16 a 19 años',
+            'A' => 'Varonil 20 a 24 años',
+            'B' => 'Varonil 25 a 29 años',
+            'C' => 'Varonil 30 a 34 años',
+            'D' => 'Varonil 35 a 39 años',
+            'E' => 'Varonil 40 a 44 años',
+            'F' => 'Varonil 45 a 49 años',
+            'G' => 'Varonil 50 a 54 años',
+            'H' => 'Varonil 55 a 59 años',
+        );
+
         $form = $this->createFormBuilder($client)
+            ->add('distance', 'choice', array('label' => 'Distancia', 'choices' => $distances))
+            ->add('category', 'choice', array('label' => 'Categoría', 'choices' => $categories))
             ->add('name', 'text', array('label' => 'Nombre(s)'))
             ->add('lastName', 'text', array('label' => 'Apellidos'))
             ->add('email', 'email', array('label' => 'Email'))
@@ -32,7 +60,35 @@ class RegistrationController extends Controller
         // Initialize Client object to handle the form
         $client = new Client();
 
+        $distances = array(
+            'SS' => 'Super Sprint'
+        );
+
+        $categories = array(
+            'JF' => 'Femenil 16 a 19 años',
+            'N' => 'Femenil 20 a 24 años',
+            'O' => 'Femenil 25 a 29 años',
+            'P' => 'Femenil 30 a 34 años',
+            'Q' => 'Femenil 35 a 39 años',
+            'R' => 'Femenil 40 a 44 años',
+            'S' => 'Femenil 45 a 49 años',
+            'T' => 'Femenil 50 a 54 años',
+            'V' => 'Femenil 55 a 59 años',
+            'IC' => 'Varonil 14 a 15 años',
+            'JV' => 'Varonil 16 a 19 años',
+            'A' => 'Varonil 20 a 24 años',
+            'B' => 'Varonil 25 a 29 años',
+            'C' => 'Varonil 30 a 34 años',
+            'D' => 'Varonil 35 a 39 años',
+            'E' => 'Varonil 40 a 44 años',
+            'F' => 'Varonil 45 a 49 años',
+            'G' => 'Varonil 50 a 54 años',
+            'H' => 'Varonil 55 a 59 años',
+        );
+
         $form = $this->createFormBuilder($client)
+            ->add('distance', 'choice', array('label' => 'Distancia', 'choices' => $distances))
+            ->add('category', 'choice', array('label' => 'Categoría', 'choices' => $categories))
             ->add('name', 'text', array('label' => 'Nombre(s)'))
             ->add('lastName', 'text', array('label' => 'Apellidos'))
             ->add('email', 'email', array('label' => 'Email'))
