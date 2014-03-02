3513af3e74ea9bc11759dde5b078e5ba63d93187
Edmundo Fuentes <me@edmundofuentes.com>
2014-03-01 19:15:33 -0600

    Adding new fields to form

--- triatlonafondo.com/src/Harcam/TriatlonBundle/Controller/RegistrationController.php
+++ triatlonafondo.com/src/Harcam/TriatlonBundle/Controller/RegistrationController.php
@@ -46,6 +46,7 @@ class RegistrationController extends Controller
             ->add('category', 'choice', array('label' => 'Categoría', 'choices' => $categories))
             ->add('name', 'text', array('label' => 'Nombre(s)'))
             ->add('lastName', 'text', array('label' => 'Apellidos'))
+            ->add('phoneNumber', 'number', array('label' => 'Teléfono'))
             ->add('email', 'email', array('label' => 'Email'))
             ->add('save', 'submit', array('label' => 'Enviar'))
             ->getForm();
@@ -91,6 +92,7 @@ class RegistrationController extends Controller
             ->add('category', 'choice', array('label' => 'Categoría', 'choices' => $categories))
             ->add('name', 'text', array('label' => 'Nombre(s)'))
             ->add('lastName', 'text', array('label' => 'Apellidos'))
+            ->add('phoneNumber', 'number', array('label' => 'Teléfono'))
             ->add('email', 'email', array('label' => 'Email'))
             ->add('save', 'submit', array('label' => 'Enviar'))
             ->getForm();
