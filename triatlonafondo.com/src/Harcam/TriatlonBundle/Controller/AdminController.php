e00f6bf98fe4e5d9b7b1311e0f81c68e1cfb0829
Edmundo Fuentes <me@edmundofuentes.com>
2014-03-01 21:43:22 -0600

    Hmm.. wrong files..

--- triatlonafondo.com/src/Harcam/TriatlonBundle/Controller/AdminController.php
+++ triatlonafondo.com/src/Harcam/TriatlonBundle/Controller/AdminController.php
@@ -45,21 +45,20 @@ class AdminController extends Controller
         // Override the ID on the URL?
         $id = $request->request->get('clientId');
         // Query the server for the club's information and parameters
-        $device = $this->getDoctrine()->getRepository('HarcamTriatlonBundle:Client')->find($id);
+        $client = $this->getDoctrine()->getRepository('HarcamTriatlonBundle:Client')->find($id);
 
-        if (!$device) {
+        if (!$client) {
             throw $this->createNotFoundException(
-                'No device found for id ' . $id
+                'No client found for id ' . $id
             );
         }
 
         // Remove from database
         $em = $this->getDoctrine()->getManager();
-        $em->remove( $device );
+        $em->remove( $client );
         $em->flush();
 
-        return $this->redirect($this->generateUrl('slambo_device_list'));
+        return $this->redirect($this->generateUrl('harcam_triatlon_admin_client_list'));
     }
-
 
 }
