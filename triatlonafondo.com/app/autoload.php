1b9bd1e7a165e56d0d0248a13395f53cf649134b
Edmundo Fuentes <me@edmundofuentes.com>
2014-03-30 13:01:04 -0600

    Updating autoload.php

--- triatlonafondo.com/app/autoload.php
+++ triatlonafondo.com/app/autoload.php
@@ -8,6 +8,8 @@ use Composer\Autoload\ClassLoader;
  */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
+$loader->add('Orderly', __DIR__.'/../vendor/orderly/src');
+
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
 return $loader;
