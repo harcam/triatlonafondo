1cd2131e5bd1b6b8e91cb1629f04a45f57819f1d
Edmundo Fuentes <me@edmundofuentes.com>
2014-03-30 16:56:34 -0600

    Fixing Categories name

--- triatlonafondo.com/src/Harcam/TriatlonBundle/Entity/Client.php
+++ triatlonafondo.com/src/Harcam/TriatlonBundle/Entity/Client.php
@@ -79,30 +79,6 @@ class Client {
      * @ORM\Column(type="string", length=2, nullable=true)
      */
     protected $status = 'N';
-    
-    public static $categories = array(
-        'IF' => 'Femenil 14 a 15 años',
-        'JF' => 'Femenil 16 a 19 años',
-        'R' => 'Femenil 20 a 24 años',
-        'O' => 'Femenil 25 a 29 años',
-        'P' => 'Femenil 30 a 34 años',
-        'Q' => 'Femenil 35 a 39 años',
-        'R' => 'Femenil 40 a 44 años',
-        'S' => 'Femenil 45 a 49 años',
-        'T' => 'Femenil 50 a 54 años',
-        'V' => 'Femenil 55 a 59 años',
-        'IC' => 'Varonil 14 a 15 años',
-        'JV' => 'Varonil 16 a 19 años',
-        'A' => 'Varonil 20 a 24 años',
-        'B' => 'Varonil 25 a 29 años',
-        'C' => 'Varonil 30 a 34 años',
-        'D' => 'Varonil 35 a 39 años',
-        'E' => 'Varonil 40 a 44 años',
-        'F' => 'Varonil 45 a 49 años',
-        'G' => 'Varonil 50 a 54 años',
-        'H' => 'Varonil 55 a 59 años',
-        'I' => 'Varonil 60 años y mayores'
-    );
 
 
     #########################
@@ -129,9 +105,33 @@ class Client {
 
     public function getCategoryName()
     {
+        $categories = array(
+            'IF' => 'Femenil 14 a 15 años',
+            'JF' => 'Femenil 16 a 19 años',
+            'R' => 'Femenil 20 a 24 años',
+            'O' => 'Femenil 25 a 29 años',
+            'P' => 'Femenil 30 a 34 años',
+            'Q' => 'Femenil 35 a 39 años',
+            'R' => 'Femenil 40 a 44 años',
+            'S' => 'Femenil 45 a 49 años',
+            'T' => 'Femenil 50 a 54 años',
+            'V' => 'Femenil 55 a 59 años',
+            'IC' => 'Varonil 14 a 15 años',
+            'JV' => 'Varonil 16 a 19 años',
+            'A' => 'Varonil 20 a 24 años',
+            'B' => 'Varonil 25 a 29 años',
+            'C' => 'Varonil 30 a 34 años',
+            'D' => 'Varonil 35 a 39 años',
+            'E' => 'Varonil 40 a 44 años',
+            'F' => 'Varonil 45 a 49 años',
+            'G' => 'Varonil 50 a 54 años',
+            'H' => 'Varonil 55 a 59 años',
+            'I' => 'Varonil 60 años y mayores'
+        );
+
         if(array_key_exists($this->category, self::categories))
         {
-            $c = self::$categories[$this->category];
+            $c = $categories[$this->category];
         } else {
             $c = 'Error en el sistema';
         }
